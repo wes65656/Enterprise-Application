e5cd3ea8d78ddec45acb413f987f5a38d64f2fd6
wes65656 <71239474+wes65656@users.noreply.github.com>
2024-12-20 17:46:47 -0300

    feat: Add Swagger configuration (main)
    Add Swagger configuration to the Identidade API project
    to generate documentation for the API endpoints.  This
    includes setup for Swagger UI and JWT Bearer
    authorization.

---
+++ NerdStoreEnterprise/src/Services/Identidade/NSE.Identidade.API/Configuration/SwaggerConfig.cs
@@ -0,0 +1,62 @@
+using System.Reflection;
+using Microsoft.OpenApi.Models;
+
+namespace NSE.Identidade.API.Configuration;
+
+public static class SwaggerConfig
+{
+    public static void AddSwaggerConfiguration(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo()
+            {
+                Title = "Projeto NSE",
+                Description = "Esse projeto é de estudo",
+                Contact = new OpenApiContact { Name = "Weslley", Email = "teste@gmail.com" }
+            });
+
+            var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+            c.IncludeXmlComments(xmlPath);
+
+            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Description = "Insira o token Jwt desta maneira; Bearer {Seu token}",
+                Name = "Authorization",
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.ApiKey
+            });
+
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    Array.Empty<string>()
+                }
+            });
+        });
+    }
+    
+    public static void UseSwaggerConfiguration(this IApplicationBuilder app)
+    { 
+        app.UseSwagger(c =>
+        {
+            c.RouteTemplate = "swagger/{documentname}/swagger.json";
+        });
+
+        app.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint("/swagger/v1/sagger.json", "v1");
+            c.RoutePrefix = "swagger";
+        });
+    }
+}
