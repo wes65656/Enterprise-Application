1b323d5bb560f318ab1b85660c4e43720823544c
wes65656 <71239474+wes65656@users.noreply.github.com>
2024-12-23 23:55:07 -0300

    feat(Identidade API): Add UserViewModels for Login and Register (main)

--- NerdStoreEnterprise/src/Services/Identidade/NSE.Identidade.API/Models/UserViewModels.cs
+++ NerdStoreEnterprise/src/Services/Identidade/NSE.Identidade.API/Models/UserViewModels.cs
@@ -2,6 +2,9 @@ using System.ComponentModel.DataAnnotations;
 
 namespace NSE.Identidade.API.Models;
 
+/// <summary>
+/// This class is used to store the user register information
+/// </summary>
 public class UserRegister
 {
     [Required(ErrorMessage = "O campo {0} é obrigatório")]
@@ -18,6 +21,9 @@ public class UserRegister
     public string ConfirmPassword { get; set; }
 }
 
+/// <summary>
+/// This class is used to store the user login information
+/// </summary>
 public class UserLogin
 {
     [Required(ErrorMessage = "O campo {0} é obrigatório")]
@@ -27,4 +33,33 @@ public class UserLogin
     [Required(ErrorMessage = "O campo {0} é obrigatório")]
     [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 6)]
     public string Password { get; set; }
+}
+
+/// <summary>
+/// This class is used to store the user answer login information
+/// </summary>
+public class UserAnswerLogin
+{
+    public string AccessToken { get; set; }
+    public double ExpiresIn { get; set; }
+    public UserToken UserToken { get; set; }
+}
+
+/// <summary>
+/// This class is used to store the user token information
+/// </summary>
+public class UserToken
+{
+    public string Id { get; set; }
+    public string Email { get; set; }
+    public IEnumerable<UserClaim> Claims { get; set; }
+}
+
+/// <summary>
+/// This class is used to store the user claim information
+/// </summary>
+public class UserClaim
+{
+    public string Value { get; set; }
+    public string Type { get; set; }
 }
