228d1df1f8f79d88c7805153f4106490bf2dc007
wes65656 <71239474+wes65656@users.noreply.github.com>
2024-12-23 23:56:07 -0300

    feat(Identidade.API): Configure JWT Authentication (main)

--- NerdStoreEnterprise/src/Services/Identidade/NSE.Identidade.API/Configuration/ApiConfig.cs
+++ NerdStoreEnterprise/src/Services/Identidade/NSE.Identidade.API/Configuration/ApiConfig.cs
@@ -1,6 +1,8 @@
+using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using NSE.Identidade.API.Data;
 using NSE.Identidade.API.Extensions;
 
@@ -33,10 +35,30 @@ public static class ApiConfig
             });
         });
 
+        // JWT
+        var appSettingsSection = configuration.GetSection("AppSettings");
+        services.Configure<AppSettings>(appSettingsSection);
+        
+        var appSettings = appSettingsSection.Get<AppSettings>();
+        var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+        
         services.AddAuthentication(options =>
         {
             options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
             options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(bearerOptions =>
+        {
+           bearerOptions.RequireHttpsMetadata = true;
+           bearerOptions.SaveToken = true;
+           bearerOptions.TokenValidationParameters = new TokenValidationParameters
+           {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes("key")),
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidAudience = appSettings.ValidIn,
+                ValidIssuer = appSettings.Issuer
+           };
         });
     }
 
