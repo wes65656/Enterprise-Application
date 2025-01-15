5900f6edfef464f33305e67b50254d0455635614
wes65656 <71239474+wes65656@users.noreply.github.com>
2025-01-14 23:53:57 -0300

    feat(NSE.WebMvc.APP): Implement user registration and login

--- NerdStoreEnterprise/src/Services/Web/MVC/NSE.WebMvc.APP/Controllers/IdentityController.cs
+++ NerdStoreEnterprise/src/Services/Web/MVC/NSE.WebMvc.APP/Controllers/IdentityController.cs
@@ -1,9 +1,50 @@
 using Microsoft.AspNetCore.Mvc;
+using NSE.WebMvc.APP.Models;
 
 namespace NSE.WebMvc.APP.Controllers;
 
 [ApiController]
 public class IdentityController : Controller
 {
+    [HttpGet]
+    [Route("Register")]
+    public IActionResult Register()
+    {
+        if (!ModelState.IsValid) return View();
+        return View();
+    }
     
+    [HttpPost]
+    [Route("new-account")]
+    public async Task<IActionResult> Register(UserRegister userRegister)
+    {
+        if (!ModelState.IsValid) return View(userRegister);
+        // registro
+        
+        if (false) return View(userRegister);
+        // realizar login na aplicação
+        
+        return RedirectToAction("Index", "Home");
+    }
+    
+    [HttpGet]
+    [Route("login")]
+    public async Task<IActionResult> Login()
+    {
+        return View();
+    }
+    
+    [HttpPost]
+    [Route("login")]
+    public async Task<IActionResult> Login(UserLogin userLogin)
+    {
+        return View();
+    }
+    
+    [HttpGet]
+    [Route("logout")]
+    public async Task<IActionResult> Logout()
+    {
+        return View();
+    }
 }
